d1413998e2095b09c9fb801e2e7b0c1ea65257b8
shtru6553 <shtru6553@3S30182.Student.UGDSB.ED>
2024-05-29 14:08:53 -0400

    Fifth Commit Completed square class

--- src/collisionvisualizer/Square.java
+++ src/collisionvisualizer/Square.java
@@ -1,13 +1,62 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+Shan Truong
+May 29, 2024
+Square class with all square functions 
  */
 package collisionvisualizer;
 
-/**
- *
- * @author shtru6553
- */
-public class Square {
+public class Square extends AbstractShape{
     
+    private double length;
+    
+    private double width;
+    
+    public Square(){
+        
+        length = 0;
+        
+        width = 0;
+    }
+    
+    public Square(double length, double width){
+        
+        this();
+        
+        this.length = length;
+        
+        this.width = width;
+    }
+    
+    public double getLength(){
+        return length;
+    }
+    
+    public double getWidth(){
+        return width;
+    }
+    
+    public void setLength(double length){
+        this.length = length;
+    }
+    
+    public void setWidth(double width){
+        this.width = width;
+    }
+    
+    public Square clone(){
+        
+        Square s = new Square(length, width);
+        
+        return s;
+    }
+
+    public boolean equals(Square s){
+        return super.equals(s) && s.length == length && s.width == width;
+    }
+
+    public String toString(){
+        
+        return super.toString() + "\nLength: " + length + "\nWidth: " + width;
+    }
+
 }
