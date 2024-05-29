0debb49d8985f7e0b77cf818e0daa87e38fc327a
arver0606 <arver0606@7KP6282.Student.UGDSB.ED>
2024-05-29 14:04:24 -0400

    Completed circle class.

--- src/collisionvisualizer/Circle.java
+++ src/collisionvisualizer/Circle.java
@@ -1,13 +1,43 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * 
+ * 
  */
 package collisionvisualizer;
 
-/**
- *
- * @author shtru6553
- */
-public class Circle {
-    
+public class Circle extends AbstractShape {
+
+    double radius;
+
+    public Circle() {
+        radius = 0;
+    }
+
+    public Circle(double radius) {
+        this();
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle clone() {
+        Circle c = new Circle(radius);
+        return c;
+    }
+
+    public boolean equals(Circle c){
+        return super.equals(c) && c.radius == radius;
+    }
+
+    public String toString(){
+        return super.toString() + "\nRadius: " + radius;
+    }
+
+
+
 }
