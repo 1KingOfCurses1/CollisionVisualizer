036a1f88e2c054d694d3eb56fb58c2b6d0e51c28
shtru6553 <shtru6553@3S30182.Student.UGDSB.ED>
2024-05-29 13:47:06 -0400

    Second Commit for all classes

--- src/collisionvisualizer/Shape.java
+++ src/collisionvisualizer/Shape.java
@@ -10,4 +10,26 @@ package collisionvisualizer;
  */
 public interface Shape {
     
+    
+    public int getXPos();
+    
+    public int getYPos();
+    
+    public double getMass();
+    
+    public double getVelocity();
+    
+    public void setXPos(int xPos);
+    
+    public void setYPos(int yPos);
+    
+    public void setMass(double mass);
+    
+    public void setVelocity(double velocity);
+    
+    public Shape clone();
+    
+    public boolean equals(Shape s);
+    
+    public String toString();
 }
