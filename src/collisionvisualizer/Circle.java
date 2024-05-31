9722b58d416c778385604852b98a06fc6ccf50ff
arver0606 <arver0606@7KP6282.Student.UGDSB.ED>
2024-05-31 13:12:50 -0400

    Commented the circle class

--- src/collisionvisualizer/Circle.java
+++ src/collisionvisualizer/Circle.java
@@ -1,67 +1,76 @@
 /*
- * 
- * 
+ * Aryan Verma
+ * 5/31/2024
+ * Circle Class
  */
 package collisionvisualizer;
 
-import java.awt.Color;
+// Imports
 import java.awt.Graphics2D;
 
 public class Circle extends AbstractShape {
-
-    private int radius;
-
-    private Color colour;
     
+    // Attributes
+    // Circle has a radius
+    private int radius;
+    
+    // Primary constructor, if nothing is provided
     public Circle() {
-        radius = 0;
-        
-        colour = new Color(0,0,0);
+        radius = 0; // Radius is set to 0
     }
-
-    public Circle(int radius, Color colour) {
+    /**
+     * Secondary constructor, if radius and color is provided
+     * @param radius - radius of the circle
+     */
+    public Circle(int radius) {
+        // Chains to the primary constructor
         this();
+        // Sets the radius
         this.radius = radius;
-        
-        this.colour = colour;
     }
-
+    /**
+     * Accessor, to get the radius of the circle
+     * @return - radius of the circle
+     */
     public int getRadius() {
         return radius;
     }
-
+    /**
+     * Mutator, sets the radius
+     * @param radius - new radius
+     */
     public void setRadius(int radius) {
         this.radius = radius;
     }
-
-    public Color getColour(){
-        return colour;
-    }
-    
-    public void setColour(Color colour){
-        this.colour = colour;
-    }
-    
+    /**
+     * Clone method, to create a copy of a circle
+     * @return - circle clone
+     */
     public Circle clone() {
-        Circle c = new Circle(radius, colour);
+        Circle c = new Circle(radius);
         return c;
     }
-
+    /**
+     * Equal method, checks if two circles are equal
+     * @param c - circle to compare to
+     * @return - true or false
+     */
     public boolean equals(Circle c){
         return super.equals(c) && c.radius == radius;
     }
-
+    /**
+     * Method that creates a string representation of a circle
+     * @return - String representation
+     */
     public String toString(){
         return super.toString() + "\nRadius: " + radius;
     }
-
-    public void draw(Graphics2D g) {
-        
-        Graphics2D g2d = (Graphics2D) g;
-
-        g2d.setPaint(colour);
-        
-        g.fillOval(xPos, yPos, radius, radius);
+    /**
+     * Draw method to draw the circle
+     * @param g2d - graphics to create the shape
+     */
+    public void draw(Graphics2D g2d) {
+        g2d.fillOval(xPos, yPos, radius, radius);
     }
 
 
